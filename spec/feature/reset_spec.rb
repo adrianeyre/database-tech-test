636ab6a40956ed4efc68beb5ea30367158c05421
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-03-06 16:52:44 +0000

    Updated reset tests

--- spec/feature/reset_spec.rb
+++ spec/feature/reset_spec.rb
@@ -1,7 +1,13 @@
 feature 'Reset all variables' do
   scenario 'I can reset all variables' do
+    visit '/set?test=123'
+    expect(page.status_code).to eq(200)
+    expect(page).to have_content ('{"test"=>"123"}')
     visit '/reset'
     expect(page.status_code).to eq(200)
     expect(page).to have_content ('Data Reset!')
+    visit '/view'
+    expect(page.status_code).to eq(200)
+    expect(page).to have_content ('{}')
   end
 end
