28def74618ae3939c6c234f397cd4501cb7f5ae6
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-03-06 13:10:43 +0000

    Tests and solution to save multiply variables

--- app/app.rb
+++ app/app.rb
@@ -7,25 +7,32 @@ class Database < Sinatra::Base
   set :session_secret, 'super secret'
   set :port, 4000
 
+  get '/reset' do
+    session["stored_hash"] = {}
+    'Data Reset!'
+  end
+
   get '/set' do
-    session["stored_hash"] ||= []
+    session["stored_hash"] ||= {}
     @passed_param = env['rack.request.query_hash']
-    p session["stored_hash"] << @passed_param
+    session["stored_hash"][@passed_param.keys[0]] = @passed_param.values[0]
     erb :set
   end
 
   get '/get' do
-    session["stored_hash"] ||= []
+    session["stored_hash"] ||= {}
     content_type :json
     @sending_param = return_hash(params["key"]).to_json
     erb :get
   end
 
   def return_hash(key)
-    session["stored_hash"].each do |hash|
-      return hash if hash[key] != nil
-    end
+    return {key=>session["stored_hash"][key]} if key_not_nil?(key)
     return {}
+  end
+
+  def key_not_nil?(key)
+    session["stored_hash"][key] != nil
   end
 
   # start the server if ruby file executed directly
