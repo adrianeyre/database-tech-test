0b2b3091dc4881d650c584feb71d9df1f51553b3
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-03-06 15:56:06 +0000

    Updated model tests

--- spec/model/database_spec.rb
+++ spec/model/database_spec.rb
@@ -1,5 +1,8 @@
 describe VariableDatabase do
   subject(:variable_database) {described_class.new}
+  let(:key)   {"somekey"}
+  let(:value) {"somevalue"}
+  let(:data)  {{key=>value}}
 
   context "Database has been initialized" do
     it "The Database initialize with a blank hash" do
@@ -8,16 +11,21 @@ describe VariableDatabase do
 
     describe '#add' do
       it 'adds data to the database' do
-        variable_database.add({"somekey"=>"somevalue"})
-        expect(variable_database.data["somekey"]).to eq "somevalue"
+        variable_database.add(data)
+        expect(variable_database.data[key]).to eq value
       end
     end
 
     describe '#get' do
       it 'gets data to the database' do
-        variable_database.add({"somekey"=>"somevalue"})
-        expect(variable_database.get("somekey")).to eq({"somekey"=>"somevalue"})
+        variable_database.add(data)
+        expect(variable_database.get(key)).to eq(data)
+      end
+
+      it 'gets empty hash if key does not exist' do
+        expect(variable_database.get(key)).to eq({})
       end
     end
+
   end
 end
